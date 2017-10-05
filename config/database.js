bd405ea34f7705cc39446688f38ce52429e6ce8d
Ahmad Irfan Mohammad Shukri <nerve2009@yahoo.com>
2017-10-05 15:53:33 +0800

    enable middlewares and create database

---
+++ config/database.js
@@ -0,0 +1,4 @@
+module.exports = {
+    database: 'mongodb://localhost:27017/demo_app',
+    secret: 'demo_app'
+}
---
+++ server.js
@@ -0,0 +1,44 @@
+let express = require('express');
+let path = require('path');
+let bodyParser = require('body-parser');
+let cors = require('cors');
+let passport = require('passport');
+let mongoose = require('mongoose');
+
+let config = require('./config/database');
+
+mongoose.connect(config.database, {useMongoClient: true});
+
+mongoose.connection.on('connected', () => {
+    console.log('connected to database ' + config.database);
+});
+
+mongoose.connection.on('error', (err) => {
+    console.log('connection error ' + err);
+})
+
+let app = express();
+
+let users = require('./routes/users');
+
+let port = process.env.PORT || 3000;
+
+// enable cors
+app.use(cors());
+
+// set static folder file
+app.use(express.static(path.join(__dirname, 'public')));
+
+// bodyparser
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.use('/users', users);
+
+app.get('/', (req, res) => {
+    res.send('Hello world');
+});
+
+app.listen(port, (req, res) => {
+    console.log('Server is running on port ' + port);
+});
